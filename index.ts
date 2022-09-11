1350878505bfc08ccb756fe8c5b627618e655b52
Rex <vivelarepublique@qq.com>
2022-09-12 03:51:11 +0800

    add generics

--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
-interface RGB {
-    r: number;
-    g: number;
-    b: number;
-    a?: number;
+interface RGB<T, U, V, W> {
+    r: T;
+    g: U;
+    b: V;
+    a?: W;
 }
 
 interface HEX {
     rgb?: string;
     rgba?: string;
 }
+
+type color = string | number;
 
 abstract class sevenSun {
     public static readonly red: string = '#FF0000';
@@ -19,17 +21,17 @@ abstract class sevenSun {
     public static readonly blue: string = '#0000FF';
     public static readonly purple: string = '#800080';
 
-    private static _calculateArray(array: number[]): string {
+    private static _calculateArray(array: color[]): string {
         try {
             let prefix = '#';
             for (let i = 0; i < array.length; i++) {
-                if (typeof array[i] !== 'number') {
+                if (typeof array[i] !== 'number' && typeof array[i] !== 'string') {
                     throw new Error(`Incorrect parameter type passed in`);
                 } else {
                     if (i < 3) {
                         prefix += this._calculateHEX(array[i]);
                     } else if (i === 3) {
-                        prefix += this._calculateHEX(array[i], 'a');
+                        prefix += this._calculateHEX(array[i], true);
                     }
                 }
             }
@@ -39,12 +41,13 @@ abstract class sevenSun {
         }
     }
 
-    private static _calculateHEX(value: number, type?: string): string {
+    private static _calculateHEX(value: color, alpha?: boolean): string {
         try {
-            if (typeof value !== 'number') {
+            if (typeof value !== 'number' && typeof value !== 'string') {
                 throw new Error(`Incorrect parameter type passed in`);
             } else {
-                if (type === 'a') {
+                value = typeof value === 'number' ? value : parseFloat(value);
+                if (alpha) {
                     if (value < 0 || value > 1) {
                         throw new Error(`The alpha value is not in the proper range`);
                     } else {
@@ -67,9 +70,9 @@ abstract class sevenSun {
         }
     }
 
-    public static toHEX(v1: RGB | string | number[] | number, v2?: number, v3?: number, v4?: number): string {
+    public static toHEX(v1: RGB<number, number, number, number> | RGB<string, string, string, string> | string | string[] | number | number[], v2?: color, v3?: color, v4?: color): string {
         try {
-            if (typeof v1 === 'string') {
+            if (typeof v1 === 'string' && (!v2 || !v3)) {
                 const matchResult = v1.match(/(?<=(rgb\()?)\d{1,3}\,\s?\d{1,3}\,\s?\d{1,3}\,\s?((0\.[0-9]{1,2})|(1))/g);
                 const rgbString = matchResult === null ? v1.match(/(?<=(rgb\()?)\d{1,3}\,\s?\d{1,3}\,\s?\d{1,3}/g) : matchResult;
                 if (!rgbString) {
@@ -83,20 +86,20 @@ abstract class sevenSun {
                     if (!v1.hasOwnProperty('a') || !v1.a) {
                         return `#${this._calculateHEX(v1.r)}${this._calculateHEX(v1.g)}${this._calculateHEX(v1.b)}`;
                     } else {
-                        return `#${this._calculateHEX(v1.r)}${this._calculateHEX(v1.g)}${this._calculateHEX(v1.b)}${this._calculateHEX(v1.a, 'a')}`;
+                        return `#${this._calculateHEX(v1.r)}${this._calculateHEX(v1.g)}${this._calculateHEX(v1.b)}${this._calculateHEX(v1.a, true)}`;
                     }
                 } else {
                     throw new Error(`Inappropriate object passed in`);
                 }
-            } else if (typeof v1 === 'object' && Array.isArray(v1)) {
+            } else if (Array.isArray(v1)) {
                 if (v1.length !== 3 && v1.length !== 4) {
                     throw new Error(`Array of incorrect length passed in`);
                 } else {
                     return this._calculateArray(v1);
                 }
-            } else if (typeof v1 === 'number' && typeof v2 === 'number' && typeof v3 === 'number') {
-                if (v4 && typeof v4 === 'number') {
-                    return `#${this._calculateHEX(v1)}${this._calculateHEX(v2)}${this._calculateHEX(v3)}${this._calculateHEX(v4, 'a')}`;
+            } else if ((typeof v1 === 'number' && typeof v2 === 'number' && typeof v3 === 'number') || (typeof v1 === 'string' && typeof v2 === 'string' && typeof v3 === 'string')) {
+                if (v4 && (typeof v4 === 'number' || typeof v4 === 'string')) {
+                    return `#${this._calculateHEX(v1)}${this._calculateHEX(v2)}${this._calculateHEX(v3)}${this._calculateHEX(v4, true)}`;
                 } else {
                     return `#${this._calculateHEX(v1)}${this._calculateHEX(v2)}${this._calculateHEX(v3)}`;
                 }
